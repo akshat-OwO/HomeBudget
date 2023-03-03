bd39250f4dd3b53244a3a57540126b6da39d2807
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-03-03 08:19:19 +0530

    Budget Cards

--- src/helper.js
+++ src/helper.js
@@ -46,3 +46,34 @@ export const createExpense = ({
 export const deleteItem = ({key}) => {
     return localStorage.removeItem(key);
 }
+
+// total spent by budget
+export const calculateSpentByBudget = (budgetId) => {
+    const expenses = fetchData('expenses') ?? [];
+    const budgetSpent = expenses.reduce((acc, expense) => {
+        // check if expense.id === budgetId I passed in
+        if (expense.budgetId !== budgetId) return acc;
+        
+        // add the current amount to total
+        return acc += expense.amount;
+    }, 0)
+    return budgetSpent;
+}
+
+// formatting
+
+// formatting percentages
+export const formatPercentage = (amt) => {
+    return amt.toLocaleString(undefined, {
+        style: 'percent',
+        minimumFractionDigits: 0
+    });
+}
+
+// format currency
+export const formatCurrency = (amt) => {
+    return amt.toLocaleString(undefined, {
+        style: 'currency',
+        currency: 'INR'
+    });
+}
